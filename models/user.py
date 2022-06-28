b06d6549d2d794f1e6baf82239ee63dcf838221d
FoleKhali <platanobrgas@gmail.com>
2022-06-28 13:50:59 -0500

    adding file user.py

---
+++ models/user.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""First user"""
+
+
+class User(BaseModel):
+    """User class with all attributes that are empty strings"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
