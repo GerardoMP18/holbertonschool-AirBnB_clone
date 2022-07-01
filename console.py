be610244ed11d717ef41d7e0bfb120e56db4a0b9
FoleKhali <platanobrgas@gmail.com>
2022-06-30 19:32:44 -0500

    adding some changes to the console.py

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 """creating the command interpreter"""
 
 from models.base_model import BaseModel
+from models import storage
 import cmd
 import json
 
@@ -16,6 +17,43 @@ class HBNBCommand(cmd.Cmd):
         'BaseModel', 'User', 'State', 'City', 'Amenity', 'Place', 'Review'
         ]
     __commands = ['all', 'count', 'create', 'destroy', 'show']
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance based 
+        on the class name and id"""
+        args = arg.split(" ")
+        if arg == '':
+            print("** class name missing **")
+        elif args[0] not in self.__classes:
+                print("** class doesn't exist **")
+        elif len(args) < 2:
+                print("** instance id missing **")
+        else:
+            id_instance = models.storage.all()
+            string = "{}.{}".format(args[0],args[1])
+            if string not in id_instance:
+                print("** no instance found **")
+            else:
+                print("{}".format(id_instance[string]))
+
+    def do_all(self, arg):
+        """Prints all string representation of 
+        all instances based or not on the class name"""
+        try:
+            list = []
+            argu = arg.split()
+
+            if argu and argu[0] not in self.__classes:
+                print("** class doesn't exist **")
+
+            for key, value in storage.all().items():
+                if (not argu or argu[0] is None or
+                        argu[0] == type(value).__name__):
+                    list.append(str(value))
+
+            print(list)
+        except Exception as exception:
+            print(exception.argu[0])
 
     def emptyline(self):
         pass
